35058466e5272bb62baaf764125824506f273731
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2024-12-18 15:01:30 +0000

    restructured around react-router-dom

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,34 +1,14 @@
-import { Container, Typography } from '@mui/material';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-
-function footerText() {
-  return <>
-    <Typography
-      variant="body2"
-      color="textSecondary"
-      align="center"
-
-    >
-
-      Development Version by Durham University COMP Group 21 - 2024
-
-    </Typography>
-  </>;
-}
-
-function loginButton() {
-  return <Button variant="contained">Login</Button>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login.js";
+import Footer from "./components/footer.js";
 
 export default function App() {
   return (
-    <Container>
-      <Box sx={{ my: 4 }}>
-        <h1>Receipt Snapper</h1>
-        {loginButton()}
-        {footerText()}
-      </ Box>
-    </Container>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+    <Footer />
   );
 }
