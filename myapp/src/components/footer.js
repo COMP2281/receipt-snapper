28b74400cb707bb1b29a68758f15f70a88d763d5
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2024-12-18 15:01:30 +0000

    restructured around react-router-dom

---
+++ myapp/src/components/footer.js
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material';
+
+export default function Footer() {
+    return <>
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        align="center"
+      >
+  
+        Development Version by Durham University COMP Group 21 - 2024
+  
+      </Typography>
+    </>;
+  }
