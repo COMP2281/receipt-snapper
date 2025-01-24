a3abb767625a52338efb6ad2b740de901dcdce4d
nael <bzxj48@durham.ac.uk>
2025-01-24 05:21:31 +0000

    Added Bootstrap for the manual input page to make it look better, added VAT and VAT Calculation in so total_amount with vat can be viewed now in admin page and made admin page a bit better (added filters, list displays, ordering by purchase date)

--- backend/manual_inputs/models.py
+++ backend/manual_inputs/models.py
@@ -5,7 +5,16 @@ class Expense(models.Model):
     merchant = models.CharField(max_length=255)
     currency = models.CharField(max_length=10)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    vat_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=20.00)  
+    vat_amount = models.DecimalField(max_digits=10, decimal_places=2, editable=False, default=0.00)  
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, editable=False, default=0.00)
     receipt_file = models.FileField(upload_to='receipts/')
+
+
+    def save(self, *args, **kwargs): # This calculates the VAT and the total amount before saving it so both options are available.
+        self.vat_amount = (self.amount * self.vat_percentage) / 100
+        self.total_amount = self.amount + self.vat_amount
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.merchant} - {self.amount} {self.currency} on {self.purchase_date}"
