a84fe7d859ef2666afb8076b97bcebb45c434762
nael <bzxj48@durham.ac.uk>
2025-01-24 05:21:31 +0000

    Added Bootstrap for the manual input page to make it look better, added VAT and VAT Calculation in so total_amount with vat can be viewed now in admin page and made admin page a bit better (added filters, list displays, ordering by purchase date)

--- backend/manual_inputs/admin.py
+++ backend/manual_inputs/admin.py
@@ -3,4 +3,8 @@ from .models import Expense
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('purchase_date', 'merchant', 'amount', 'currency')  
+    list_display = ('purchase_date', 'merchant', 'amount', 'vat_percentage', 'vat_amount', 'total_amount', 'currency')  
+    list_filter = ('purchase_date', 'currency', 'vat_percentage')  
+    search_fields = ('merchant', 'currency')  
+    ordering = ('-purchase_date',)
+    readonly_fields = ('vat_amount', 'total_amount') #these are Read-Only fields. 
--- backend/manual_inputs/forms.py
+++ backend/manual_inputs/forms.py
@@ -4,7 +4,13 @@ from .models import Expense
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ['purchase_date', 'merchant', 'amount', 'currency', 'receipt_file']
+        fields = ['purchase_date', 'merchant', 'amount','vat_percentage', 'currency', 'receipt_file']
         widgets = {
             'purchase_date': forms.DateInput(attrs={'type': 'date'}), 
+            'vat_percentage': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'placeholder': 'Enter VAT (%)'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
