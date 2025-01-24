e6456206dd9a490217c429bb4b3659c360a55acf
nael <bzxj48@durham.ac.uk>
2025-01-24 04:49:49 +0000

    Fixed admin page so its now functional (Can be used to view manually submitted forms too) and made manual input page functionality a bit better

--- backend/manual_inputs/admin.py
+++ backend/manual_inputs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from .models import Expense  
 
-# Register your models here.
+@admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('purchase_date', 'merchant', 'amount', 'currency')
+    list_display = ('purchase_date', 'merchant', 'amount', 'currency')  
