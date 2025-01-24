fda6826e5e2ab7f319eabaad7920ea2dd9e9f159
nael <bzxj48@durham.ac.uk>
2025-01-24 04:16:17 +0000

    Fixed manual_input page and fixed form code

--- backend/manual_inputs/views.py
+++ backend/manual_inputs/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Expense
 from .forms import ExpenseForm
 
 def manual_input(request):
+    form = ExpenseForm()
     if request.method == 'POST':
         form = ExpenseForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponse("Expense saved successfully") # Can be changed to redirect to a possible success page.
-    else:
-        form = ExpenseForm()
-    return render(request, 'manualinputs.html') 
+            return render(request, 'manual_input/success.html')
+        
+    print(form)
+    return render(request, 'manual_input/manual_input.html', {'form': form})
