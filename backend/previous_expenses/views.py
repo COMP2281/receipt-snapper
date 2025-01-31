bc4d6b1a980c2af6a4f71ba93086b5fa60514989
sharona2 <147186704+sharona2@users.noreply.github.com>
2025-01-31 10:32:16 +0000

    Update views.py

--- backend/previous_expenses/views.py
+++ backend/previous_expenses/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils.timezone import now
+from .models import Expense
+from .serializers import ExpenseSerializer
 
 # Create your views here.
+class ExpenseViewSet(ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    @action(detail=False, methods=['get'])
+    def previous_expenses(self, request):
+        previous_expenses = self.get_queryset().filter(purchase_date__lt=now().date())
+        serializer = self.get_serializer(previous_expenses, many=True)
+        return Response(serializer.data)
