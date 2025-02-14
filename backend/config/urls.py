23f3ad83aad994c9569211b4934622cc825cf69b
nael <bzxj48@durham.ac.uk>
2025-02-14 12:15:57 +0000

    renamed backend to config

---
+++ backend/config/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+#from django.http import HttpResponse
+from django.contrib import admin
+from django.urls import path, include
+from manual_inputs import views
+
+#def root_view(request):
+#    return HttpResponse("Root test")
+
+urlpatterns = [
+    #path('', views.index),     #root link to react
+    #path('', root_view, name='root'),  #root
+    path('admin/', admin.site.urls),    #admin url
+    path('', include('myapi.urls')), #api url
+    #path('', include('myapi.urls')),
+    path('login_system/', include('login_system.urls')),
+    path('api/', include('manual_inputs.urls')),  
+]
