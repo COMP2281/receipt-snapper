0e6afa667555225ea5a3bd7635800a3f24cdb2e8
sharona2 <147186704+sharona2@users.noreply.github.com>
2025-02-21 10:18:44 +0000

    Update tests.py

--- backend/previous_expenses/tests.py
+++ backend/previous_expenses/tests.py
@@ -1,3 +1,27 @@
+from datetime import timedelta
+from django.utils.timezone import now
 from django.test import TestCase
+from .models import PreviousExpense
 
-# Create your tests here.
+class PreviousExpensesTest(TestCase):
+#old expense
+    def test_previous_expenses_filter(self):
+        PreviousExpense.objects.create(
+            purchase_date=now().date() - timedelta(days=10),  
+            merchant="Old Merchant",
+            amount=50.00,
+            vat_percentage=10.00,
+            currency="USD"
+        )
+
+#future expense
+        PreviousExpense.objects.create(
+            purchase_date=now().date() + timedelta(days=5),  
+            merchant="Future Merchant",
+            amount=75.00,
+            vat_percentage=5.00,
+            currency="EUR"
+        )
+
+        previous_expenses = PreviousExpense.objects.filter(purchase_date__lt=now().date())
+        self.assertEqual(previous_expenses.count(), 1)
