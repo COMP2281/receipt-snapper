37ff8a671330dcda69418823f938cc1be4401f24
sharona2 <147186704+sharona2@users.noreply.github.com>
2025-02-21 10:15:46 +0000

    Update serializers.py

--- backend/previous_expenses/serializers.py
+++ backend/previous_expenses/serializers.py
@@ -3,4 +3,6 @@ from .models import PreviousExpense
 
 class PreviousExpenseSerializer(serializers.ModelSerializer):
   class meta:
-    
+        model = PreviousExpense
+        fields = ['id', 'purchase_date', 'merchant', 'amount', 'vat_percentage', 'vat_amount', 'total_amount', 'currency', 'receipt_file']
+        read_only_fields = ['vat_amount', 'total_amount'] 
