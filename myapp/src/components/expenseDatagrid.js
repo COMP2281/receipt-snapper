7b12c7d5986391d9fb80c33489e464d3a96b8e09
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-02 19:40:45 +0000

    first draft of datagrid completed

--- myapp/src/components/expenseDatagrid.js
+++ myapp/src/components/expenseDatagrid.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Typography, useTheme } from "@mui/material";
 import { DataGrid } from '@mui/x-data-grid';
+import ProgressTag from "./progressTag";
 
 export default function ExpenseDatagrid({ rows }) {
     const theme = useTheme();
     return (
         <DataGrid
             sx={{
-                '& .MuiDataGrid-row': {
-                    cursor: 'pointer',
-                },
                 '& .MuiDataGrid-row:hover': {
                     backgroundColor: 'rgba(255, 255, 255, 0.06)',
                 },
@@ -19,9 +17,18 @@ export default function ExpenseDatagrid({ rows }) {
                 '& .MuiButtonBase-root': {
                     color: '#FFFFFF',
                 },
+                '& .MuiDataGrid-columnHeader': {
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                },
+                '& .MuiDataGrid-columnSeparator': {
+                    opacity: .6,
+                },
                 '& .MuiDataGrid-toolbarContainer': {
-                    backgroundColor: '#424242',
-                    borderBottom: '1px solid rgba(255, 255, 255, 0.12)',
+                    backgroundColor: theme.palette.background.paper,
+                },
+                '& .MuiDataGrid-cell': {
+                    borderTop: '1px solid rgba(255, 255, 255, 0.2)',
                 },
                 // Add a drop shadow to the datagrid
                 boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.47)',
@@ -31,9 +38,28 @@ export default function ExpenseDatagrid({ rows }) {
             rows={rows}
             columns={[
                 {
+                    field: '',
+                    headerName: ' ',
+                    width: 10,
+                    resizable: false,
+                    sortable: false,
+                    filterable: false,
+                    disableColumnMenu: true,
+                    renderCell: (params) => (
+                        <Typography sx={{
+                            p: 1.25,
+                            fontSize: '1rem',
+                        }}>
+                            💷
+                        </Typography>
+                    )
+                },
+                {
                     field: 'date',
                     headerName: 'Date',
                     width: 100,
+                    headerAlign: 'center',
+                    align: 'center',
                 },
                 {
                     field: 'description',
@@ -52,16 +78,62 @@ export default function ExpenseDatagrid({ rows }) {
                     headerName: 'Amount',
                     width: 100,
                     editable: false,
+                    headerAlign: 'center',
+                    align: 'center',
+                    valueFormatter: (params, row) => {
+                        const value = parseInt(params) / 100;
+                        const currency = row.currency || 'GBP';
+                        return new Intl.NumberFormat('en-GB', {
+                            style: 'currency',
+                            currency: currency,
+                        }).format(value);
+                    }
+                },
+                {
+                    field: 'currency',
+                    headerName: 'Currency',
+                    width: 75,
+                    editable: false,
+                    resizable: false,
+                    align: 'center',
+                    headerAlign: 'center',
                 },
                 {
                     field: 'status',
                     headerName: 'Status',
                     width: 150,
                     editable: false,
+                    headerAlign: 'center',
+                    align: 'center',
+                    renderCell: (params) => {
+                        const colors = {
+                            'Completed': '#4CAF50', // green
+                            'Waiting': '#FFC107', // amber
+                            'Processing': '#2196F3', // blue
+                            'Queued': '#9C27B0', // purple
+                            'Failed': '#F44336' // red
+                        }
+
+                        const progresses = {
+                            'Completed': 100,
+                            'Waiting': 75,
+                            'Processing': 50,
+                            'Queued': 25,
+                            'Failed': 100
+                        }
+                        
+                        const statusColor = colors[params.value] || '#888888';
+                        const statusProgress = progresses[params.value] || 100;
+
+                        return <ProgressTag text={params.value} color={statusColor} progress={statusProgress} />;
+                    }
                 }
-            ]}
+            ]} 
             pageSize={5}
             rowsPerPageOptions={[5]}
+            rowHeight={45}
+            
+
         />
     );
 }
