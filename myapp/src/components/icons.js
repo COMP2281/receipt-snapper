acf232be10d5f37a2e33446a76ef4967c61ca8bc
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-02 17:53:52 +0000

    added drop shadow and animation to logo

--- myapp/src/components/icons.js
+++ myapp/src/components/icons.js
@@ -8,6 +8,15 @@ export default function RSIcon() {
         alt="Receipt Snapper Icon"
         sx={{
             height: '50px',
+            filter: 'drop-shadow(0px 4px 10px rgba(0, 0, 0, 0.3))',
+            pointer: 'pointer',
+            transition: 'transform 0.5s',
+            '&:hover': {
+                transform: 'rotate(-5deg) scale(1.1)',
+
+            },
+
         }}
+        onClick={() => window.location.href = '/dashboard'}
     />
 };
