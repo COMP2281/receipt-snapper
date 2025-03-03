7fabe1cceeef0e6f4ad462e74c58c049d2a83267
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-03 16:48:13 +0000

    removed react-pdf, doesn't meet requirements

--- myapp/src/pages/edit.js
+++ myapp/src/pages/edit.js
@@ -1,14 +1,9 @@
 import React, { useEffect } from "react";
 import { Container, Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import DocViewer, { PDFRenderer, JPGRenderer, PNGRenderer } from "react-doc-viewer";
 
 export default function Edit({ transaction }) {
     const theme = useTheme();
-
-    const docs = [
-        { uri: "/assets/dev/example-invoice.pdf"},
-    ];
 
     return (
         <Container maxWidth={false} disableGutters sx={{ mt: -2, display: 'flex', flexDirection: { xs: "column", md: "row" }, alignItems: 'stretch', height: 'calc(100vh - 72px)', position: 'relative' }}>
@@ -23,44 +18,6 @@ export default function Edit({ transaction }) {
                 textAlign: 'center',
                 justifyContent: 'center',
             }}>
-                <DocViewer documents={docs} pluginRenderers={ [PDFRenderer, JPGRenderer, PNGRenderer] } theme=
-                    {{
-                        primary: theme.palette.primary.main,
-                        secondary: theme.palette.secondary.main,
-                        disableThemeScrollbar: true,
-                        disableGutters: true,
-                    }}
-                    config=
-                    {{
-                        header: {
-                            disableHeader: true,
-                        },
-                        sidebar: {
-                            disableSidebar: true,
-                        },
-                        page: {
-                            disablePage: true,
-                        },
-                        controls: {
-                            disableControls: false,
-                        },
-                        search: {
-                            disableSearch: true,
-                        },
-                        navigation: {
-                            disableNavigation: false,
-                        },
-                        rotation: {
-                            disableRotation: false,
-                        },
-                        zoom: {
-                            disableZoom: false,
-                        },
-
-                    }}
-                />
-                
-                
             </Box>
             <Box sx={{
                 backgroundColor: '#222222',
