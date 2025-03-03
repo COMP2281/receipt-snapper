780cc93f51925f3c72be2ed5b2d9600e69d0fd9f
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-03 16:07:24 +0000

    added document viewer

--- myapp/src/pages/edit.js
+++ myapp/src/pages/edit.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Container, Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+
 <Box sx={{ mt: 0, height: '100%' }}></Box>
 export default function Edit({ transaction }) {
     const theme = useTheme();
+
+    const docs = [
+        { uri: "/assets/dev/example-receipt.png"},
+    ];
 
     return (
         <Container maxWidth={false} disableGutters sx={{ mt: -2, display: 'flex', flexDirection: { xs: "column", md: "row" }, alignItems: 'stretch', height: 'calc(100vh - 72px)', position: 'relative' }}>
@@ -15,8 +21,47 @@ export default function Edit({ transaction }) {
                 display: 'flex',
                 p: 3,
                 m: 0,
+                textAlign: 'center',
+                justifyContent: 'center',
             }}>
-                IMAGE
+                <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} theme=
+                    {{
+                        primary: theme.palette.primary.main,
+                        secondary: theme.palette.secondary.main,
+                        disableThemeScrollbar: true,
+                        disableGutters: true,
+                    }}
+                    config=
+                    {{
+                        header: {
+                            disableHeader: true,
+                        },
+                        sidebar: {
+                            disableSidebar: true,
+                        },
+                        page: {
+                            disablePage: true,
+                        },
+                        controls: {
+                            disableControls: false,
+                        },
+                        search: {
+                            disableSearch: true,
+                        },
+                        navigation: {
+                            disableNavigation: false,
+                        },
+                        rotation: {
+                            disableRotation: false,
+                        },
+                        zoom: {
+                            disableZoom: false,
+                        },
+
+                    }}
+                />
+                
+                
             </Box>
             <Box sx={{
                 backgroundColor: '#222222',
