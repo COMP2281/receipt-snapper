69cbcf4bab88cefe3212b179be1922bb741846bb
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-03 14:53:54 +0000

    started edit page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import Login from "./pages/login.js";
 import Dashboard from "./pages/dashboard.js";
+import Edit from "./pages/edit.js";
 import Error from "./pages/error.js";
 import Footer from "./components/footer.js";
 import Navbar from "./components/navbar.js";
@@ -11,12 +12,13 @@ import Navbar from "./components/navbar.js";
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ mt: 10}}>
+      <Box sx={{ mt: 10, height: '100%'}}>
         <Router>
         <Navbar />
-          <Routes>
+          <Routes sx={{ height: '100%' }}>
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/edit" element={<Edit transaction="1" />} />
             <Route path="*" element={<Error code="404" />} />
           </Routes>
         </Router>
