42d12b015c869a68172ecb5dc6d89f764a9bbafb
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-05 21:12:52 +0000

    added sx passthrough to PageDescription react element

--- myapp/src/components/pageDescription.js
+++ myapp/src/components/pageDescription.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Typography } from "@mui/material";
 
-export default function PageDescription({ text }) {
+export default function PageDescription({ text, sx }) {
     return (
         <Typography sx={{
-            
+            sx
         }}>
             {text}
         </Typography>
