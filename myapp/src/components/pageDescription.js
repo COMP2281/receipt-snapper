01fe3c4a7327a421418152eb2b7713c3751bf0d4
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-05 21:14:40 +0000

    gave description its own box element

--- myapp/src/components/pageDescription.js
+++ myapp/src/components/pageDescription.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function PageDescription({ text, sx }) {
     return (
-        <Typography sx={{
-            sx
-        }}>
-            {text}
-        </Typography>
+        <Box sx={{...sx}}>
+            <Typography>
+                {text}
+            </Typography>
+        </Box>
     );
 }
