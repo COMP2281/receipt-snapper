7381407bba11cb373c577353c6f61ab37b1b3de3
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-08 10:56:22 +0000

    deleted existing structure to bring in old parts piece by piece

--- backend/upload/models.py
+++
@@ -1,7 +0,0 @@
-
-from django.db import models
-
-class Blob(models.Model):
-    blob_name = models.CharField(max_length=255, unique=True)
-    blob_url = models.URLField()
-    uploaded_at = models.DateTimeField(auto_now_add=True)
--- backend/upload/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Blob
-
-class BlobSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Blob
-        fields = 'all'
