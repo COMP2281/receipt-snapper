e5cea0fda267198befb3a5b4172ac155f53e87ae
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-11 12:34:48 +0000

    got database setup fully and got static files serving correctly, well as admin assets

--- backend/apps/card_data/models.py
+++ backend/apps/card_data/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, primary_key=True)
+
+class Location(models.Model):
+    code = models.CharField(max_length=10, primary_key=True)
+
+class CardData(models.Model):
+    date = models.DateField()
+    currency_code = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_exchange_rate = models.DecimalField(max_digits=10, decimal_places=6)
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    exchange_override = models.CharField(max_length=50)
+    location_code = models.ForeignKey(Location, on_delete=models.CASCADE)
--- backend/apps/expenses/models.py
+++ backend/apps/expenses/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from card_data.models import Currency, Location, CardData
+from users.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class Project(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Status(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
+class Expense(models.Model):
+    date = models.DateField()
+    description = models.TextField(null=True, blank=True)
+    company_paid = models.CharField(max_length=50, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency_code = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    location_code = models.ForeignKey(Location, on_delete=models.CASCADE)
+    line_item = models.ForeignKey(CardData, on_delete=models.SET_NULL, null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
