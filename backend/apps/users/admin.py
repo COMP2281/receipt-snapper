dfb43b3a16da76726a995e4518181984ed14095d
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-11 13:03:46 +0000

    recreated database with new info

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+class CustomUserAdmin(DefaultUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'card_no')
+    fieldsets = (
+        (None, {'fields': ('password', 'card_no')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        ("New User Data", {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'card_no')}
+        ),
+    )
+
+# Unregister the default User model and register the custom User model
+admin.site.register(User, CustomUserAdmin)
