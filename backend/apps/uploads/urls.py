ff982e2d7350828371d5b3ba70400db9885162f0
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-13 16:18:27 +0000

    got blob uploads working

--- backend/apps/uploads/urls.py
+++ backend/apps/uploads/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import UploadBlobView
 
 urlpatterns = [
-    # Add your URL patterns here
+    path('', UploadBlobView.as_view(), name='upload_blob'),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, get_user_model
+from django.conf import settings
 
-# Create your views here.
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+User = get_user_model()
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        try:
+            user_instance = User.objects.get(email=email)
+            user = authenticate(request, username=user_instance.username, password=password)
+        except User.DoesNotExist:
+            user = None
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user_id': user.pk,
+                'email': user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+            })
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
+class InfoView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'user_id': user.pk,
+            'email': user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        })
