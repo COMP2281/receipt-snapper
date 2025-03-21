78c564fcab3ed75e4f120670408c7d12e2aae870
Riley Hutton <80292379+rileyhutton@users.noreply.github.com>
2025-03-21 01:32:51 +0000

    added simple tax amount calculation

--- backend/apps/exports/views.py
+++ backend/apps/exports/views.py
@@ -61,7 +61,7 @@ class ExportExpensesView(APIView):
 
 
 
-                0,  # Total Tax Amount is not present in the model
+                f"{(expense.amount / 100)*.2:.2f}"
                 f"{expense.amount / 100:.2f}",  # Assuming Net Amount is the same as Amount
                 expense.project.id if expense.project else '',
                 expense.project.name if expense.project else ''
